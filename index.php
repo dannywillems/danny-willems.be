0360399c74b75b8cddf3af429a5d91e7b3b76897
Danny Willems <contact@danny-willems.be>
2015-07-05 23:01:39 +0200

    update

--- index.php
+++ index.php
@@ -1,138 +1,184 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Danny Willems</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+<!----------------------------------------------------------------------------->
+		<!-- FRAMEWORKS -->
+		<!-- Bootstrap css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<!-- JQuery -->
+		<script src="js/jquery-1.11.3.min.js"></script>
+		<!-- Bootstrap js -->
+		<script src="js/bootstrap.min.js"></script>
+<!----------------------------------------------------------------------------->
+		<link rel="stylesheet" href="css/main.css">
+	</head>
 
-<title>Danny Willems - Developer</title>
-<meta name="description" content="" />
-<meta name="keywords" content="" />
-<meta name="author" content="" />
+	<body>
+<!----------------------------------------------------------------------------->
+		<!-- NAV BAR -->
+		<div class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<center>
+					<ul class="nav navbar-nav">
+						<li class="active">
+							<a href="http://danny-willems.be">Home</a>
+						</li>
+						<li>
+							<a href="http://danny-willems.be/projects.php">Projets</a>
+						</li>
+						<li>
+							<a href="http://danny-willems.be/blog.php">Blog</a>
+						</li>
+						<li>
+							<a href="http://danny-willems.be/experience.php">Expérience</a>
+						</li>
+						<li>
+							<a href="http://danny-willems.be/cv.php">CV</a>
+						</li>
+						<li>
+							<a href="http://danny-willems.be/contact.php">Contact</a>
+						</li>
+					</ul>
+				</center>
+			</div>
+		</div>
+<!----------------------------------------------------------------------------->
+		<!-- Resume -->
+		<div class="resume">
+			<div class="container marketing">
+				<div class="row">
+					<div class="col-lg-3">
+						<img src="img/personal.jpg" class="img-responsive img-circle" alt="personal" />
+					</div>
+					<div class="col-lg-9">
+						<center>
+							<p class="small-description">
+								Danny Willems, 20 ans, développeur Web et logiciel, Administrateur système.
+							</p>
+						</center>
+					</div>
+				</div>
+			</div>
+		</div>
+<!----------------------------------------------------------------------------->
+		<!-- Skills Resume -->
+		<div class="container marketing">
+			<div class="row skills-resume">
+				<center>
+					<h1 class="title-skills-resume">
+						Administrateur système
+					</h1>
+				</center>
+				<div class="col-lg-4">
+					<center>
+						<img src="img/Linux_platform.png" class="img-responsive img-rounded" alt="linux_color" />
+						<img src="img/Free_BSD_color.png" class="img-responsive img-rounded" alt="freebsd_color" />
+						<h2 class="name-skills-resume">
+							Linux et Unix.
+						</h2>
+						<p>
+							Gestion de serveurs privés: je gère en
+							continu plusieurs serveurs privés tournant
+							sous des distributions Linux (Debian et
+							Ubuntu server) et anciennement sous FreeBSD.
+						</p>
+						<p>
+							Gestion d'un parc Linux utilisateur: ayant
+							fait découvrir la philosophie open-source à
+							mon entourage, je suis en charge de gérer
+							leur distribution Linux au quotidien.
+						</p>
+					</center>
+				</div>
+				<div class="col-lg-4">
+					<center>
+						<img src="img/terminal_color.png" class="img-responsive img-rounded" alt="terminal" />
+						<h2 class="name-skills-resume">
+							Shell scripting
+						</h2>
+						<p>
+							Pour automatiser les tâches, je suis amené à écrire des scripts shell et à utiliser cron ainsi que ses variantes.
+							La plupart des tâches que je réalise passent par le terminal.
+						</p>
+						<p>
+							Je maitrise également vim, celui-ci étant mon éditeur de texte et mon IDE dans chaque environnement et pour chaque langage.
+						</p>
+					</center>
+				</div>
 
-<script type="text/javascript" src="swfobject.js"></script>
-<script type="text/javascript">
-	var flashvars = {};
-	var params = {
-		menu: "true",
-		quality: "high",
-		scale: "noscale",
-		salign: "tl",
-		wmode: "window",
-		bgcolor: "#ffffff",
-		allowscriptaccess: "always"
-	};
-	var attributes = {id:"flashcontent"};
-	swfobject.embedSWF("sapling.swf", "flashcontent", "100%", "100%", "9", false, flashvars, params, attributes);
-</script>
+				<div class="col-lg-4">
+					<center>
+						<img src="img/network.png" />
+					</center>
+				</div>
+			</div>
+			<div class="row skills-resume">
+				<center>
+					<h1 class="title-skills-resume">
+						Développement logiciel
+					</h1>
+				</center>
+				<!-- CONTENTS -->
+			</div>
 
-<!-- Begin iPhone/iPad detection code -->
-<script type="text/javascript">
-	if ((navigator.userAgent.indexOf('iPhone') != -1) || (navigator.userAgent.indexOf('iPod') != -1) || (navigator.userAgent.indexOf('iPad') != -1)) {
-		document.location = "mobile/";
-	}
-</script>
-<!-- End iPhone/iPad detection code -->
+			<div class="row skills-resume">
+				<center>
+					<h1 class="title-skills-resume">
+						Développement Web
+					</h1>
+				</center>
+				<div class="col-lg-4">
+					<center>
+						<img src="img/front-end_html5_css3_js_bootstrap.png" class="img-responsive img-rounded" alt="front-end" />
+						<h2 class="name-skills-resume">
+							Front-end
+						</h2>
+						<p>
+							Maitrise de HTML5, CSS3 et JavaScript.
+						</p>
+						<p>
+							Framework CSS: Bootstrap
+						</p>
+						<p>
+							Framework JS: JQuery
+						</p>
+					</center>
+				</div>
+				<div class="col-lg-4">
+					<center>
+						<img src="img/back-end_php_mysql.png" class="img-responsive img-rounded" alt="back-end" />
+						<h2 class="name-skills-resume">
+							Back-end
+						</h2>
+						<p>
+						</p>
+					</center>
+				</div>
+				<div class="col-lg-4">
+					<center>
+						<img src="img/servers_128.png" class="img-responsive img-rounded" alt="server" />
+						<h2 class="name-skills-resume">
+							Hébergement, administration et maintenance
+						</h2>
+						<p>
+							Ne vous préoccupez plus de la maintenance de votre siteweb et de votre serveur.
+						</p>
+					</center>
+				</div>
+			</div>
+		</div>
+<!----------------------------------------------------------------------------->
+		<hr>
+<!----------------------------------------------------------------------------->
+		<hr>
+<!----------------------------------------------------------------------------->
 
-<style type="text/css">
-
-html {
-	height: 100%;
-	overflow: hidden;
-}
-
-body {
-	height: 100%;
-	margin: 0;
-	padding: 0;
-	background-color: #f5f9ed;
-	font-family: Arial, sans-serif;
-	color: #333333;
-}
-
-a, a:visited, a:link {
-	color: #07c2a8;
-	text-decoration: underline;
-}
-
-a:active, a:hover {
-	text-decoration: none;
-}
-
-#flashcontent {
-	width: 100%;
-	height: 100%;
-}
-
-#noflash {
-	width: 320px;
-	margin: 0;
-	padding: 0;
-}
-
-h1 {
-	font-size: 30px;
-	letter-spacing: -2px;
-	margin-bottom: -10px;
-}
-
-h2 {
-	font-size: 20px;
-}
-
-p {
-	font-size: 11px;
-}
-
-#footer p {
-	font-size: 11px;
-}
-
-#content, #footer {
-	padding: 20px;
-}
-
-#upgrade {
-	background-color: #ffffff;
-	padding: 20px;
-	margin-top: 20px;
-	margin-bottom: 20px;
-	border: 1px dotted #cccccc;
-}
-
-</style>
-
-</head>
-<body>
-
-<div id="flashcontent">
-	
-	<div id="noflash">
-		
-		<div id="content">
-		
-			<h1>Danny Willems</h1>
-			<h2>Developer</h2>
-			
-			<div id="upgrade">
-			
-				<p><strong>Adobe Flash Player 9 is required to view my site</strong></p>
-				<p><a href="http://www.adobe.com/go/getflashplayer/">&raquo; Download the latest version here</a></p>
-				<p><a href="mobile/">&raquo; Visit a non-Flash version of my website</a></p>
-			
-			</div><!-- / #upgrade -->
-			
-			<p><a href="mailto:contact@danny-willems.be">contact@danny-willems.be</a></p>
-		
-		</div><!-- / #content -->
-		
-		<div id="footer">
-			
-			<p>2015 Danny Willems</p>
-		
-		</div><!-- / #footer -->
-		
-	</div><!-- / #noflash -->
-	
-</div><!-- / #flashcontent -->
-
-</body>
+<!----------------------------------------------------------------------------->
+		<hr>
+<!----------------------------------------------------------------------------->
+		<!-- Project List "en alternance" -->
+	</body>
 </html>
