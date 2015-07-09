b85690679410fc566ebc9069131ffe0eb102b87c
Danny Willems <contact@danny-willems.be>
2015-07-09 18:01:23 +0200

    default message

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 		<!-- Bootstrap js -->
 		<script src="js/bootstrap.min.js"></script>
 <!----------------------------------------------------------------------------->
+<!-- You can access to the not-minified less file in http://danny-willems.be/less/main.less -->
 		<link rel="stylesheet" href="css/main.css">
 	</head>
 
@@ -21,28 +22,26 @@
 		<!-- NAV BAR -->
 		<div class="navbar navbar-inverse">
 			<div class="container-fluid">
-				<center>
-					<ul class="nav navbar-nav">
-						<li class="active">
-							<a href="http://danny-willems.be">Home</a>
-						</li>
-						<li>
-							<a href="http://danny-willems.be/projects.php">Projets</a>
-						</li>
-						<li>
-							<a href="http://danny-willems.be/blog.php">Blog</a>
-						</li>
-						<li>
-							<a href="http://danny-willems.be/experience.php">Expérience</a>
-						</li>
-						<li>
-							<a href="http://danny-willems.be/cv.php">CV</a>
-						</li>
-						<li>
-							<a href="http://danny-willems.be/contact.php">Contact</a>
-						</li>
-					</ul>
-				</center>
+				<ul class="nav navbar-nav">
+					<li class="active">
+						<a href="http://danny-willems.be">Home</a>
+					</li>
+					<li>
+						<a href="http://danny-willems.be/projects.php">Projets</a>
+					</li>
+					<li>
+						<a href="http://danny-willems.be/blog.php">Blog</a>
+					</li>
+					<li>
+						<a href="http://danny-willems.be/experience.php">Expérience</a>
+					</li>
+					<li>
+						<a href="http://danny-willems.be/cv.php">CV</a>
+					</li>
+					<li>
+						<a href="http://danny-willems.be/contact.php">Contact</a>
+					</li>
+				</ul>
 			</div>
 		</div>
 <!----------------------------------------------------------------------------->
@@ -54,11 +53,24 @@
 						<img src="img/personal.jpg" class="img-responsive img-circle" alt="personal" />
 					</div>
 					<div class="col-lg-9">
-						<center>
-							<p class="small-description">
-								Danny Willems, 20 ans, développeur Web et logiciel, Administrateur système.
-							</p>
-						</center>
+						<div class="resume-presentation">
+								<header>
+									Danny Willems
+								</header>
+								<p>
+									Etudiant actuellement les sciences
+									mathématiques (MA1) à l'<a
+										href="http://umons.ac.be">université de
+										Mons (UMONS)</a>, j'ai toujours eu une
+									passion pour l'informatique et la physique.
+									Bien qu'ayant poursuivi une spécialité physique
+									pendant mes 3 premières années, je me suis
+									formé en auto-didacte à l'informatique.
+								</p>
+								<p>
+									Je 
+								</p>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -67,118 +79,150 @@
 		<!-- Skills Resume -->
 		<div class="container marketing">
 			<div class="row skills-resume">
-				<center>
-					<h1 class="title-skills-resume">
-						Administrateur système
-					</h1>
-				</center>
+				<header>
+					Administrateur système
+				</header>
 				<div class="col-lg-4">
-					<center>
-						<img src="img/Linux_platform.png" class="img-responsive img-rounded" alt="linux_color" />
-						<img src="img/Free_BSD_color.png" class="img-responsive img-rounded" alt="freebsd_color" />
-						<h2 class="name-skills-resume">
-							Linux et Unix.
-						</h2>
-						<p>
-							Gestion de serveurs privés: je gère en
-							continu plusieurs serveurs privés tournant
-							sous des distributions Linux (Debian et
-							Ubuntu server) et anciennement sous FreeBSD.
-						</p>
-						<p>
-							Gestion d'un parc Linux utilisateur: ayant
-							fait découvrir la philosophie open-source à
-							mon entourage, je suis en charge de gérer
-							leur distribution Linux au quotidien.
-						</p>
-					</center>
+					<img src="img/Linux_platform.png" class="img-responsive img-rounded" alt="linux_color" />
+					<img src="img/Free_BSD_color.png" class="img-responsive img-rounded" alt="freebsd_color" />
+					<header>
+						Linux et Unix.
+					</header>
+					<p>
+						Gestion de serveurs privés: je gère en
+						continu plusieurs serveurs privés tournant
+						sous des distributions Linux (Debian et
+						Ubuntu server) et anciennement sous FreeBSD.
+					</p>
+					<p>
+						Gestion d'un parc Linux utilisateur: ayant
+						fait découvrir la philosophie open-source à
+						mon entourage, je suis en charge de gérer
+						leur distribution Linux au quotidien.
+					</p>
 				</div>
 				<div class="col-lg-4">
-					<center>
-						<img src="img/terminal_color.png" class="img-responsive img-rounded" alt="terminal" />
-						<h2 class="name-skills-resume">
-							Shell scripting
-						</h2>
-						<p>
-							Pour automatiser les tâches, je suis amené à écrire des scripts shell et à utiliser cron ainsi que ses variantes.
-							La plupart des tâches que je réalise passent par le terminal.
-						</p>
-						<p>
-							Je maitrise également vim, celui-ci étant mon éditeur de texte et mon IDE dans chaque environnement et pour chaque langage.
-						</p>
-					</center>
+					<img src="img/terminal_color.png" class="img-responsive img-rounded" alt="terminal" />
+					<header>
+						Shell scripting
+					</header>
+					<p>
+						Pour automatiser les tâches, je suis amené à écrire des scripts shell et à utiliser cron ainsi que ses variantes.
+						La plupart des tâches que je réalise passent par le terminal.
+					</p>
+					<p>
+						Je maitrise également vim, celui-ci étant mon éditeur de texte et mon IDE dans chaque environnement et pour chaque langage.
+					</p>
 				</div>
 
 				<div class="col-lg-4">
-					<center>
-						<img src="img/network.png" />
-					</center>
+					<img src="img/network.png" />
+					<header>
+						Réseau
+					</header>
+					<p>
+
+					</p>
 				</div>
-			</div>
-			<div class="row skills-resume">
-				<center>
-					<h1 class="title-skills-resume">
-						Développement logiciel
-					</h1>
-				</center>
-				<!-- CONTENTS -->
 			</div>
 
 			<div class="row skills-resume">
-				<center>
-					<h1 class="title-skills-resume">
-						Développement Web
-					</h1>
-				</center>
+				<header>
+					Développement logiciel
+				</header>
+			</div>
+
+			<div class="row skills-resume">
+				<header>
+					Développement Web
+				</header>
 				<div class="col-lg-4">
-					<center>
-						<img src="img/front-end_html5_css3_js_bootstrap.png" class="img-responsive img-rounded" alt="front-end" />
-						<h2 class="name-skills-resume">
-							Front-end
-						</h2>
-						<p>
-							Maitrise de HTML5, CSS3 et JavaScript.
-						</p>
-						<p>
-							Framework CSS: Bootstrap
-						</p>
-						<p>
-							Framework JS: JQuery
-						</p>
-					</center>
+					<img src="img/front-end_html5_css3_js_bootstrap.png" class="img-responsive img-rounded" alt="front-end" />
+					<header>
+						Front-end
+					</header>
+					<p>
+						Maitrise de HTML5, CSS3 et JavaScript.
+					</p>
+					<p>
+						Framework CSS: Bootstrap
+					</p>
+					<p>
+						Framework JS: JQuery
+					</p>
 				</div>
 				<div class="col-lg-4">
-					<center>
-						<img src="img/back-end_php_mysql.png" class="img-responsive img-rounded" alt="back-end" />
-						<h2 class="name-skills-resume">
-							Back-end
-						</h2>
-						<p>
-						</p>
-					</center>
+					<img src="img/back-end_php_mysql.png" class="img-responsive img-rounded" alt="back-end" />
+					<header>
+						Back-end
+					</header>
+					<p>
+
+					</p>
 				</div>
 				<div class="col-lg-4">
-					<center>
-						<img src="img/servers_128.png" class="img-responsive img-rounded" alt="server" />
-						<h2 class="name-skills-resume">
+					<img src="img/servers_128.png" class="img-responsive img-rounded" alt="server" />
+					<header>
 							Hébergement, administration et maintenance
-						</h2>
-						<p>
-							Ne vous préoccupez plus de la maintenance de votre siteweb et de votre serveur.
-						</p>
-					</center>
+					</header>
+					<p>
+						Ne vous préoccupez plus de la maintenance de votre siteweb et de votre serveur.
+					</p>
 				</div>
 			</div>
 		</div>
 <!----------------------------------------------------------------------------->
 		<hr>
 <!----------------------------------------------------------------------------->
-		<hr>
-<!----------------------------------------------------------------------------->
+		<div class="container marketing">
+			<div class="row prices">
+				<header>
+					Prix et distinctions
+				</header>
+				<div class="col-lg-4">
+					<img />
+				</div>
+				<div class="col-lg-8">
+					<article>
+						<header>
+							Hackathon <em>be-hackathon</em>
+						</header>
+					</article>
+				</div>
+			</div>
 
+			<div class="row prices">
+				<div class="col-lg-8">
+					<article>
+						<header>
+							Hackathon <em>Société Générale</em>
+						</header>
+					</article>
+				</div>
+				<div class="col-lg-4">
+					<img />
+				</div>
+			</div>
+		</div>
 <!----------------------------------------------------------------------------->
 		<hr>
 <!----------------------------------------------------------------------------->
-		<!-- Project List "en alternance" -->
+		<div class="container marketing">
+			<div class="row projects">
+				<header>
+					Projets personnels
+				</header>
+				<div class="col-lg-4">
+					<img />
+				</div>
+				<div class="col-lg-8">
+					<article>
+						<header>
+							vim-IDE
+						</header>
+					</article>
+				</div>
+			</div>
+<!----------------------------------------------------------------------------->
 	</body>
 </html>
