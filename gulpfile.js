527530ba7092677c4e707e6e2a256c72a27ee6b9
Danny Willems <contact@danny-willems.be>
2015-12-06 09:45:46 +0100

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var less = require('gulp-less');
 var minify_css = require('gulp-minify-css');
 var watch = require('gulp-watch');
 
+var l_dest_dir = "./css";
+var l_src_dir = "./less/**/*.less";
+
 function compile_and_compress_less()
 {
-	var l_dest_dir = "./css";
-	var l_src_dir = "./less/**/*.less";
-
 	return (gulp.src(l_src_dir)
 				.pipe(less())
 				.pipe(minify_css())
@@ -18,5 +18,10 @@ gulp.task("compile_and_compress_less", function()
 		{
 			return (compile_and_compress_less());
 		});
+
+gulp.task("watch", function()
+        {
+            gulp.watch(l_src_dir, ["compile_and_compress_less"]);
+        });
 
 gulp.task("default", ["compile_and_compress_less"]);
