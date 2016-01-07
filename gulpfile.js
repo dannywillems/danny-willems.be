16a334118cc120c51aa1cb4c6f4e3a912c830e05
Danny Willems <contact@danny-willems.be>
2016-01-07 20:15:45 +0100

    update gulpfile.js by separating tasks, functions and variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Variables for files */
+var less_files = "./less/*.less";
+var css_dir = "css/";
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/* Dependancies */
 var gulp = require('gulp');
 var less = require('gulp-less');
 var minify_css = require('gulp-minify-css');
 var watch = require('gulp-watch');
+/*----------------------------------------------------------------------------*/
 
-var l_dest_dir = "./css";
-var l_src_dir = "./less/**/*.less";
-
+/*----------------------------------------------------------------------------*/
+/* Functions for tasks */
 function compile_and_compress_less()
 {
-	return (gulp.src(l_src_dir)
+	return (gulp.src(less_files)
 				.pipe(less())
 				.pipe(minify_css())
-				.pipe(gulp.dest(l_dest_dir)));
+				.pipe(gulp.dest(css_dir)));
 }
 
-gulp.task("compile_and_compress_less", function()
-		{
-			return (compile_and_compress_less());
-		});
+function fn_watch()
+{
+    gulp.watch(less_files, ["compile_and_compress_less"]);
+}
+/*----------------------------------------------------------------------------*/
 
-gulp.task("watch", function()
-        {
-            gulp.watch(l_src_dir, ["compile_and_compress_less"]);
-        });
+/*----------------------------------------------------------------------------*/
+/* Tasks */
+gulp.task("compile_and_compress_less", compile_and_compress_less);
+gulp.task("watch", fn_watch);
 
 gulp.task("default", ["compile_and_compress_less"]);
+/*----------------------------------------------------------------------------*/
