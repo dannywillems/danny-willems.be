01e9fc8140f74c011f5f60b844e15efe26c1110b
Danny Willems <contact@danny-willems.be>
2016-05-01 23:48:41 +0200

    change name of OCaml cordova bindings

--- index.php
+++ index.php
@@ -270,7 +270,7 @@
         <div class="container">
             <div class="row">
               <?php
-                new_card("OCaml cordova plugins bindings", "img/ocaml-cordova.png", "OCaml and Cordova", $lang['PP_OCAML_CORDOVA_PLACE'], $lang['PP_OCAML_CORDOVA_DESC'], $lang['PP_OCAML_CORDOVA_BLOG_LINK'], "");
+                new_card("OCaml cordova plugins bindings", "img/ocaml-cordova.png", "OCaml + Cordova", $lang['PP_OCAML_CORDOVA_PLACE'], $lang['PP_OCAML_CORDOVA_DESC'], $lang['PP_OCAML_CORDOVA_BLOG_LINK'], "");
                 new_card("Vim-IDE vim ide plugins vim-ide", "img/vim-ide.png", "Vim-IDE", $lang['PP_VIM_IDE_PLACE'], $lang['PP_VIM_IDE_DESC'], $lang['PP_VIM_IDE_BLOG_LINK'], "");
                 new_card("GSLib ocaml python c library", "img/gslib.png", "GSLib", $lang['PP_GSLIB_PLACE'], $lang['PP_GSLIB_DESC'], $lang['PP_GSLIB_BLOG_LINK'], "");
                 new_card("GSMath ocaml python c library", "img/gsmath.png", "GSMath", $lang['PP_GSMATH_PLACE'], $lang['PP_GSMATH_DESC'], $lang['PP_GSMATH_BLOG_LINK'], "");
